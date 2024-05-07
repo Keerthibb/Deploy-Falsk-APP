ce59436f6dbd3e50039f1820abb20dfcd50b7de7
Keerthibb <92278245+Keerthibb@users.noreply.github.com>
2024-05-07 06:25:22 -0700

    Add files via upload

---
+++ terraform.tfvars
@@ -0,0 +1,6 @@
+region = "us-east-1"
+cidr = "10.0.0.0/16"
+availability_zone = "us-east-1a"
+public_cidr = "10.0.0.0/24"
+ami = "ami-080e1f13689e07408"
+instance_type = "t2.micro"
---
+++ variable.tf
@@ -0,0 +1,23 @@
+variable "region" {
+  description = "value for region"
+}
+
+variable "cidr" {
+    description = "value for cidr"
+}
+
+variable "availability_zone" {
+    description = "value for zone "
+}
+
+variable "public_cidr" {
+    description = "value for public zone "
+}
+
+variable "ami" {
+  default = "value for ami"
+}
+
+variable "instance_type" {
+  description = "value for instance"
+}
