de70bd2e7ebd907341f891e6c82f4b87b7aa729c
Keerthibb <92278245+Keerthibb@users.noreply.github.com>
2024-05-07 06:58:58 -0700

    Add files via upload

---
+++ Flask-Infra/variable.tf
@@ -0,0 +1,27 @@
+variable "region" {
+  description = "value for region"
+}
+
+variable "cidr" {
+    description = "value for cidr"
+}
+
+variable "availability_zone" {
+    description = "value for zone "
+}
+
+variable "public_cidr" {
+    description = "value for public zone "
+}
+
+variable "ami" {
+  default = "value for ami"
+}
+
+variable "instance_type" {
+  description = "value for instance"
+}
+
+variable "public_key" {
+  description = "public key for ec2 instance"
+}
