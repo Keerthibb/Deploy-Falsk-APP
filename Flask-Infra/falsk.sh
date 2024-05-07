dde9e5852f76dba573d88a3157acd691336d3d7b
Keerthibb <92278245+Keerthibb@users.noreply.github.com>
2024-05-07 07:31:45 -0700

    Update falsk.sh

--- Flask-Infra/falsk.sh
+++ Flask-Infra/falsk.sh
@@ -4,6 +4,7 @@ export NEEDRESTART_SUSPEND=1
 sudo apt update -y
 sudo NEEDRESTART_SUSPEND=1 apt install -y python3-pip
 cd /home/ubuntu/
+git clone 
 sudo pip3 install -r requirements.txt
 cd /home/ubuntu/
 sudo python3 app.py &
